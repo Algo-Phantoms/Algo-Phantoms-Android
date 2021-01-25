17364af44ec7cb1509ff3f75f4913278c6b66843
Nakul Sharma <rnqnkl@gmail.com>
2021-01-25 15:12:53 +0530

    Added login activity.

--- app/src/main/java/com/algo/phantoms/algo_phantom/MainActivity.kt
+++ app/src/main/java/com/algo/phantoms/algo_phantom/MainActivity.kt
@@ -2,10 +2,18 @@ package com.algo.phantoms.algo_phantom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import meow.bottomnavigation.MeowBottomNavigation
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigation = findViewById<MeowBottomNavigation>(R.id.bottom_nav)
+        bottomNavigation.add(MeowBottomNavigation.Model(1, R.drawable.ic_pathway))
+        bottomNavigation.add(MeowBottomNavigation.Model(2, R.drawable.ic_code))
+        bottomNavigation.add(MeowBottomNavigation.Model(3, R.drawable.ic_person))
+
+        bottomNavigation.show(1)
     }
 }
