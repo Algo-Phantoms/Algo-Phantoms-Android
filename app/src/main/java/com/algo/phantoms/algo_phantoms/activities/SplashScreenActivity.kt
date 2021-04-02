0a7ddd1941a45365cdcaee1a6b1d7dabd7d78f83
SHREYASH ASATI <62322907+Shreyash41@users.noreply.github.com>
2021-04-02 18:37:20 +0530

    Adding SplashScreen (#22)
    * Adding SplashScreen
    
    * Update SplashScreen
    
    * Update SplashScreen

--- app/src/main/java/com/algo/phantoms/algo_phantoms/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/algo/phantoms/algo_phantoms/activities/SplashScreenActivity.kt
@@ -1,12 +1,32 @@
 package com.algo.phantoms.algo_phantoms.activities
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.algo.phantoms.algo_phantoms.R
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashScreenActivity, LoginActivity::class.java))
+            finish()
+        },
+                2500);
     }
 }
