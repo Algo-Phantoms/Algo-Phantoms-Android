419fe4b04eec5d7a01e5e045761b122a2ea325fc
AasheeshLikePanner <ashishlearningsoftware@gmail.com>
2023-09-04 22:01:02 +0530

    Adding Code for Changing text color in dark mode

--- app/src/main/java/com/algo/phantoms/algo_phantoms/fragments/CodeFragment.kt
+++ app/src/main/java/com/algo/phantoms/algo_phantoms/fragments/CodeFragment.kt
@@ -1,5 +1,6 @@
 package com.algo.phantoms.algo_phantoms.fragments
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -22,11 +23,28 @@ class CodeFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_code, container, false)
         binding = FragmentCodeBinding.bind(view)
-        binding.editor.setSyntaxHighlightRules(
-            SyntaxHighlightRule("[0-9]*", "#6512c4"),
-            SyntaxHighlightRule("[^A-Za-z0-9]","#F30E0B"),
-            SyntaxHighlightRule("[A-Za-z]",  "#242322")
-        )
+
+
+        val DarkModeFlags = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        val isDarkModeOn = DarkModeFlags == Configuration.UI_MODE_NIGHT_YES
+
+        if (isDarkModeOn) {
+            binding.editor.setSyntaxHighlightRules(
+                SyntaxHighlightRule("[0-9]*", "#6512c4"),
+                SyntaxHighlightRule("[^A-Za-z0-9]","#F30E0B"),
+                SyntaxHighlightRule("[A-Za-z]",  "#ffffff")
+            )
+        }
+        else{
+            binding.editor.setSyntaxHighlightRules(
+                SyntaxHighlightRule("[0-9]*", "#6512c4"),
+                SyntaxHighlightRule("[^A-Za-z0-9]","#F30E0B"),
+                SyntaxHighlightRule("[A-Za-z]",  "#242322")
+            )
+        }
+
+
+
         return view
     }
 
